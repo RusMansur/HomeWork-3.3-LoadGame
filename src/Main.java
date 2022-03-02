5aa8e7e1859f7dfdde5dfb20b3bcba844e7f554b
Руслан Долгополов <rusdolg@gmail.com>
2022-03-02 18:27:03 +0300

    Добавлена загрузка файла сохранения

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -17,6 +18,12 @@ public class Main {
                 FileOutputStream fileOutputStream = new FileOutputStream(USERPATH + "savegames/" + name);
                 for (int f = zipInputStream.read(); f != -1; f = zipInputStream.read()) {
                     fileOutputStream.write(f);
+                }
+                try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(USERPATH + "savegames/" + name))) {
+                    GameProgress gameProgress = (GameProgress) objectInputStream.readObject();
+                    System.out.println(gameProgress.toString());
+                } catch (ClassNotFoundException | IOException ex) {
+                    System.err.println("Не удалось прочитать файл " + name);
                 }
                 fileOutputStream.flush();
                 zipInputStream.closeEntry();
